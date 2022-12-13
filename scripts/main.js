98906e93c1193b2cb67566e385f5397775689d50
Zakwan <hishamzakwan@gmail.com>
2022-12-13 17:43:53 +0800

    adding some js script to make website more responsive ;)

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,37 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello, World!";
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello, World!";
 
-/*
-let iceCream = "mango";
-if (iceCream === "chocolate") {
-  alert("Yay, I love chocolate ice cream!");
-} else {
-  alert("Awwww, but chocolate is my favorite…");
-}
-*/
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "../assets/img/Firefox_logo,_2019.svg.png") {
+    myImage.setAttribute("src", "../assets/img/Firefox-Logo-PNG-Image.png");
+  } else {
+    myImage.setAttribute("src", "../assets/img/Firefox_logo,_2019.svg.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+};
+
+myButton.onclick = () => {
+  setUserName();
+};
